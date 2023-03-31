012384429b0b405f646c6786631a9c7ce19af947
Georgina Edwards <118028272+Notunique1@users.noreply.github.com>
2023-03-30 23:54:25 -0400

    Initial commit

--- client/main.js
+++ client/main.js
@@ -23,3 +23,15 @@ const getFortune = () => {
 
 fortuneBtn.addEventListener('click', getFortune)
 
+
+
+const handleSubmit = (e) =>
+{
+  e.preventDefault(); // prevent reload while click submit button
+  const name = e.target.name.value;
+  const email = e.target.email.value;
+  const password = e.target.password.value;
+  // do something
+}
+var element = document.getElementById("Mobility");
+element.value = "10";
